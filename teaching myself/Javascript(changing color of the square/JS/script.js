26dde3cc4f23a059f8d7296960fa0105ebe3c4ca
Majed Zarkash <majedzarkash@gmail.com>
2024-11-19 13:40:56 +1100

    .

---
+++ teaching myself/Javascript(changing color of the square/JS/script.js
@@ -0,0 +1,12 @@
+let button = document.querySelector('#button');
+
+
+function changeColor(){
+    
+    square.style.backgroundColor = 'red';
+}
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+    //let square = ;
+    console.log(document.getElementById('square').style.backgroundColor)
+  });
